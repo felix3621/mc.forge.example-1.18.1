456d4efed0de7fef73d72d942f84ddc814bf7fb3
Boogaloo3621 <fjellvangsander@gmail.com>
2023-06-16 11:46:27 +0200

    8: Advanced items

--- src/main/java/io/github/felix3621/example/item/ModItems.java
+++ src/main/java/io/github/felix3621/example/item/ModItems.java
@@ -1,6 +1,7 @@
 package io.github.felix3621.example.item;
 
 import io.github.felix3621.example.Example;
+import io.github.felix3621.example.item.custom.DowsingRodItem;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
@@ -15,6 +16,9 @@ public class ModItems {
             () -> new Item(new Item.Properties().tab(ModCreativeModeTab.EXAMPLE_TAB)));
     public static final RegistryObject<Item> RAW_CITRINE = ITEMS.register("raw_citrine",
             () -> new Item(new Item.Properties().tab(ModCreativeModeTab.EXAMPLE_TAB)));
+
+    public static final RegistryObject<Item> DOWSING_ROD = ITEMS.register("dowsing_rod",
+            () -> new DowsingRodItem(new Item.Properties().tab(ModCreativeModeTab.EXAMPLE_TAB).durability(16)));
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
