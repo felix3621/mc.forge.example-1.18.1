3dca4a1cd4f59d0bf615ad7fe9d6039bc690314d
Boogaloo3621 <fjellvangsander@gmail.com>
2023-06-16 13:27:46 +0200

    13: Tags

--- src/main/java/io/github/felix3621/example/item/custom/DowsingRodItem.java
+++ src/main/java/io/github/felix3621/example/item/custom/DowsingRodItem.java
@@ -1,5 +1,6 @@
 package io.github.felix3621.example.item.custom;
 
+import io.github.felix3621.example.util.ModTags;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.core.BlockPos;
 import net.minecraft.network.chat.Component;
@@ -67,7 +68,6 @@ public class DowsingRodItem extends Item {
     }
 
     private boolean isValuableBlock(Block block) {
-        return block == Blocks.COAL_ORE || block == Blocks.COPPER_ORE
-                || block == Blocks.DIAMOND_ORE || block == Blocks.IRON_ORE;
+        return ModTags.Blocks.DOWSING_ROD_VALUABLES.contains(block);
     }
 }
---
+++ src/main/java/io/github/felix3621/example/util/ModTags.java
@@ -0,0 +1,34 @@
+package io.github.felix3621.example.util;
+
+import io.github.felix3621.example.Example;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.BlockTags;
+import net.minecraft.tags.ItemTags;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+import net.minecraftforge.common.Tags;
+
+public class ModTags {
+    public static class Blocks {
+        public static final Tags.IOptionalNamedTag<Block> DOWSING_ROD_VALUABLES
+                = tag("dowsing_rod_valuables");
+
+        private static Tags.IOptionalNamedTag<Block> tag(String name) {
+            return BlockTags.createOptional(new ResourceLocation(Example.MOD_ID, name));
+        }
+        private static Tags.IOptionalNamedTag<Block> forgeTag(String name) {
+            return BlockTags.createOptional(new ResourceLocation("forge", name));
+        }
+    }
+
+    public static class Items {
+        public static final Tags.IOptionalNamedTag<Item> CITRINE_GEMS = forgeTag("gems/citrine");
+
+        private static Tags.IOptionalNamedTag<Item> tag(String name) {
+            return ItemTags.createOptional(new ResourceLocation(Example.MOD_ID, name));
+        }
+        private static Tags.IOptionalNamedTag<Item> forgeTag(String name) {
+            return ItemTags.createOptional(new ResourceLocation("forge", name));
+        }
+    }
+}
