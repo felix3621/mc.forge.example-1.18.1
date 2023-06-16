89cf1292eb0098b2d7101dc1dbd1b83b12955000
Boogaloo3621 <fjellvangsander@gmail.com>
2023-06-16 12:09:01 +0200

    10: Custom food

---
+++ src/main/java/io/github/felix3621/example/item/ModFoods.java
@@ -0,0 +1,7 @@
+package io.github.felix3621.example.item;
+
+import net.minecraft.world.food.FoodProperties;
+
+public class ModFoods {
+    public static final FoodProperties CUCUMBER = (new FoodProperties.Builder()).fast().nutrition(2).saturationMod(0.2F).build();
+}
--- src/main/java/io/github/felix3621/example/item/ModItems.java
+++ src/main/java/io/github/felix3621/example/item/ModItems.java
@@ -20,6 +20,9 @@ public class ModItems {
     public static final RegistryObject<Item> DOWSING_ROD = ITEMS.register("dowsing_rod",
             () -> new DowsingRodItem(new Item.Properties().tab(ModCreativeModeTab.EXAMPLE_TAB).durability(16)));
 
+    public static final RegistryObject<Item> CUCUMBER = ITEMS.register("cucumber",
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.EXAMPLE_TAB).food(ModFoods.CUCUMBER)));
+
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
     }
