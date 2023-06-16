6f01f40b8ccdaf8fd8437d61cbc835bbfe784935
Boogaloo3621 <fjellvangsander@gmail.com>
2023-06-16 17:18:04 +0200

    22: Animated textures

--- src/main/java/io/github/felix3621/example/item/ModItems.java
+++ src/main/java/io/github/felix3621/example/item/ModItems.java
@@ -59,6 +59,10 @@ public class ModItems {
             () -> new ArmorItem(ModArmorMaterials.CITRINE, EquipmentSlot.FEET,
                     new Item.Properties().tab(ModCreativeModeTab.EXAMPLE_TAB)));
 
+    public static final RegistryObject<Item> MAGIC_DUST = ITEMS.register("magic_dust",
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.EXAMPLE_TAB)));
+
+
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
     }
