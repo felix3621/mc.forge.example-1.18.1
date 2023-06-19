05867b664113819405f04f5240bc7caf067656ec
Boogaloo3621 <fjellvangsander@gmail.com>
2023-06-19 19:52:38 +0200

    55: Entity variants

--- src/main/java/io/github/felix3621/example/entity/client/RaccoonModel.java
+++ src/main/java/io/github/felix3621/example/entity/client/RaccoonModel.java
@@ -3,7 +3,10 @@ package io.github.felix3621.example.entity.client;
 import io.github.felix3621.example.Example;
 import io.github.felix3621.example.entity.custom.RaccoonEntity;
 import net.minecraft.resources.ResourceLocation;
+import software.bernie.geckolib3.core.event.predicate.AnimationEvent;
+import software.bernie.geckolib3.core.processor.IBone;
 import software.bernie.geckolib3.model.AnimatedGeoModel;
+import software.bernie.geckolib3.model.provider.data.EntityModelData;
 
 public class RaccoonModel extends AnimatedGeoModel<RaccoonEntity> {
     @Override
@@ -13,11 +16,24 @@ public class RaccoonModel extends AnimatedGeoModel<RaccoonEntity> {
 
     @Override
     public ResourceLocation getTextureLocation(RaccoonEntity object) {
-        return new ResourceLocation(Example.MOD_ID, "textures/entity/raccoon/raccoon.png");
+        return RaccoonRenderer.LOCATION_BY_VARIANT.get(object.getVariant());
     }
 
     @Override
     public ResourceLocation getAnimationFileLocation(RaccoonEntity animatable) {
         return new ResourceLocation(Example.MOD_ID, "animations/raccoon.animation.json");
+    }
+
+    @SuppressWarnings({ "unchecked", "rawtypes" })
+    @Override
+    public void setLivingAnimations(RaccoonEntity entity, Integer uniqueID, AnimationEvent customPredicate) {
+        super.setLivingAnimations(entity, uniqueID, customPredicate);
+        IBone head = this.getAnimationProcessor().getBone("head");
+
+        EntityModelData extraData = (EntityModelData) customPredicate.getExtraDataOfType(EntityModelData.class).get(0);
+        if (head != null) {
+            head.setRotationX(extraData.headPitch * ((float) Math.PI / 180F));
+            head.setRotationY(extraData.netHeadYaw * ((float) Math.PI / 180F));
+        }
     }
 }
