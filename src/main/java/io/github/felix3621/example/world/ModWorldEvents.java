fe9b2307bcd20eeda0191d0814eb41d2153c89f5
Boogaloo3621 <fjellvangsander@gmail.com>
2023-06-19 20:41:09 +0200

    56: Entity world generation

--- src/main/java/io/github/felix3621/example/world/ModWorldEvents.java
+++ src/main/java/io/github/felix3621/example/world/ModWorldEvents.java
@@ -1,9 +1,14 @@
 package io.github.felix3621.example.world;
 
 import io.github.felix3621.example.Example;
+import io.github.felix3621.example.entity.ModEntityTypes;
+import io.github.felix3621.example.world.gen.ModEntityGeneration;
 import io.github.felix3621.example.world.gen.ModFlowerGeneration;
 import io.github.felix3621.example.world.gen.ModOreGeneration;
 import io.github.felix3621.example.world.gen.ModTreeGeneration;
+import net.minecraft.world.entity.SpawnPlacements;
+import net.minecraft.world.entity.animal.Animal;
+import net.minecraft.world.level.levelgen.Heightmap;
 import net.minecraftforge.event.world.BiomeLoadingEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -16,5 +21,7 @@ public class ModWorldEvents {
 
         ModTreeGeneration.generateTrees(event);
         ModFlowerGeneration.generateFlowers(event);
+
+        ModEntityGeneration.onEntitySpawn(event);
     }
 }
