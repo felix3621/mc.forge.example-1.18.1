32423bf3dd7fdce29627688635903ce5dc460f32
Boogaloo3621 <fjellvangsander@gmail.com>
2023-06-21 16:31:55 +0200

    69-1: mistake: client setup on main

--- src/main/java/io/github/felix3621/example/event/ModEventClientBusEvents.java
+++ src/main/java/io/github/felix3621/example/event/ModEventClientBusEvents.java
@@ -1,14 +1,30 @@
 package io.github.felix3621.example.event;
 
 import io.github.felix3621.example.Example;
+import io.github.felix3621.example.block.ModBlocks;
 import io.github.felix3621.example.block.entity.ModBlockEntities;
+import io.github.felix3621.example.block.entity.ModWoodTypes;
 import io.github.felix3621.example.block.entity.client.AnimatedBlockRenderer;
+import io.github.felix3621.example.entity.ModEntityTypes;
+import io.github.felix3621.example.entity.client.RaccoonRenderer;
 import io.github.felix3621.example.entity.client.armor.CitrineArmorRenderer;
+import io.github.felix3621.example.fluid.ModFluids;
 import io.github.felix3621.example.item.custom.CitrineArmorItem;
+import io.github.felix3621.example.screen.GemCuttingStationScreen;
+import io.github.felix3621.example.screen.ModMenuTypes;
+import io.github.felix3621.example.util.ModItemProperties;
+import net.minecraft.client.gui.screens.MenuScreens;
+import net.minecraft.client.renderer.ItemBlockRenderTypes;
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.client.renderer.blockentity.BlockEntityRenderers;
+import net.minecraft.client.renderer.blockentity.SignRenderer;
+import net.minecraft.client.renderer.entity.EntityRenderers;
+import net.minecraft.world.level.block.state.properties.WoodType;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.EntityRenderersEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import software.bernie.geckolib3.renderers.geo.GeoArmorRenderer;
 
 @Mod.EventBusSubscriber(modid = Example.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
@@ -21,5 +37,37 @@ public class ModEventClientBusEvents {
     @SubscribeEvent
     public static void registerRenderers(final EntityRenderersEvent.RegisterRenderers event) {
         event.registerBlockEntityRenderer(ModBlockEntities.ANIMATED_BLOCK_ENTITY.get(), AnimatedBlockRenderer::new);
+    }
+
+    @SubscribeEvent
+    public static void clientSetup(final FMLClientSetupEvent event) {
+        ItemBlockRenderTypes.setRenderLayer(ModBlocks.EBONY_DOOR.get(), RenderType.translucent());
+        ItemBlockRenderTypes.setRenderLayer(ModBlocks.EBONY_TRAPDOOR.get(), RenderType.translucent());
+
+        ItemBlockRenderTypes.setRenderLayer(ModBlocks.PINK_ROSE.get(), RenderType.cutout());
+        ItemBlockRenderTypes.setRenderLayer(ModBlocks.POTTED_PINK_ROSE.get(), RenderType.cutout());
+
+        ItemBlockRenderTypes.setRenderLayer(ModBlocks.WINTER_WINDOW.get(), RenderType.translucent());
+        ItemBlockRenderTypes.setRenderLayer(ModBlocks.CUCUMBER_PLANT.get(), RenderType.cutout());
+
+        ItemBlockRenderTypes.setRenderLayer(ModBlocks.EBONY_LEAVES.get(), RenderType.cutout());
+        ItemBlockRenderTypes.setRenderLayer(ModBlocks.EBONY_SAPLING.get(), RenderType.cutout());
+
+        ItemBlockRenderTypes.setRenderLayer(ModBlocks.GEM_CUTTING_STATION.get(), RenderType.translucent());
+
+        ItemBlockRenderTypes.setRenderLayer(ModFluids.HONEY_BLOCK.get(), RenderType.translucent());
+        ItemBlockRenderTypes.setRenderLayer(ModFluids.HONEY_FLUID.get(), RenderType.translucent());
+        ItemBlockRenderTypes.setRenderLayer(ModFluids.HONEY_FLOWING.get(), RenderType.translucent());
+
+        ModItemProperties.addCustomItemProperties();
+
+        MenuScreens.register(ModMenuTypes.GEM_CUTTING_STATION_MENU.get(), GemCuttingStationScreen::new);
+
+        WoodType.register(ModWoodTypes.EBONY);
+        BlockEntityRenderers.register(ModBlockEntities.SIGN_BLOCK_ENTITIES.get(), SignRenderer::new);
+
+        EntityRenderers.register(ModEntityTypes.RACCOON.get(), RaccoonRenderer::new);
+
+        ItemBlockRenderTypes.setRenderLayer(ModBlocks.FELIX_PORTAL.get(), RenderType.translucent());
     }
 }
